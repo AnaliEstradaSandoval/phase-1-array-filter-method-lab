1cfe2b24fb8ab26d3a2837e62840e8ae4c9ff36a
Anali Estrada Sandoval <analiestrada44@gmail.com>
2023-04-22 21:40:50 -0700

    array filter lab completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+function findMatching(drivers, driverName){
+    let findDriver = drivers.filter((name) =>
+    name.toLowerCase().includes(driverName.toLowerCase()))
+    return findDriver
+}
+
+
+function fuzzyMatch(drivers, query){
+    let string = drivers.filter(function (element){
+        if(query[0,1] === element[0,1]){
+            return element
+        }
+    })
+    return string
+}
+
+
+function matchName(drivers, string){
+    let driverInfo = drivers.filter((element) => {
+        if (string === element.name){
+            return element
+        }
+    })
+    return driverInfo
+}
+
+
